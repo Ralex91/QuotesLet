49bb9d4a69e5f4ea1959b1f4a483a745e4f73725
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-02-27 21:06:33 +0100

    chore(): add seed quote queries

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO quote (text) VALUES 
+('Je ne suis pas paresseux, je suis en mode économie d''énergie.'),
+('La vie est courte. Souris pendant que tu as encore des dents.'),
+('Je ne suis pas en retard, les autres sont juste en avance.'),
+('Je mange du gâteau parce que quelqu''un, quelque part, est au régime.'),
+('Je ne suis pas têtu, je suis juste très déterminé.'),
+('Je garde mon corps comme un temple. Malheureusement, c''est un temple ancien en ruines.'),
+('Le café et moi, c''est une histoire d''amour. Avec des hauts, des bas, et beaucoup de caféine.'),
+('Mon lit me demande de rester avec lui chaque matin. Nous avons une relation vraiment forte.'),
+('Je ne vieillis pas, je me bonifie.'),
+('Le monde appartient à ceux qui se lèvent tôt. Mais le lit appartient à ceux qui restent dedans.');
